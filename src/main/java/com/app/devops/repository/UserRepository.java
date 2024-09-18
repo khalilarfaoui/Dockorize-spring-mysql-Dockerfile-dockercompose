8d0d465fc3562847d83aad95fdd1f453a74b623e
khalilarfaoui <khalilwin1920@gmail.com>
2024-09-18 11:55:04 +0100

    first

---
+++ src/main/java/com/app/devops/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.devops.repository ;
+import com.app.devops.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
